68c441e5c1a7800986415a151984abd3412f6585
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-02-21 15:24:04 -0500

    Add Person class to example
    Co-authored-by: Stinacon <Stinacon@users.noreply.github.com>
    Co-authored-by: Jeeglobs <Jeeglobs@users.noreply.github.com>
    Co-authored-by: Michael Greason <MichaelGreason@users.noreply.github.com>
    Co-authored-by: GerardoRoman <GerardoRoman@users.noreply.github.com>
    Co-authored-by: Candace Andrews <CandaceAndrews@users.noreply.github.com>

--- classes.py
+++ classes.py
@@ -12,3 +12,23 @@ class Pet:
     def call(self):
         # self refers to the specific instance of Pet that you are calling
         print(f'Come here, {self.name}!')
+
+
+class Person:
+    def __init__(self, provided_name='anonymous', provided_age=None):
+        # giving the parameters default values also turns them into
+        # keyword arguments
+        self.name = provided_name
+        self.age = provided_age
+
+    def greet(self):
+        print("Hello, {self.name}")
+
+
+# Example, create a Person record for Rihanna
+riri = Person(provided_age=32, provided_name="Rihanna")
+# with keyword arguments, the order you pass them does not matter
+backup_dancer1 = Person(provided_age=24)
+# because we gave default values to the arguments, they are not required
+backup_dancer1.name = "Christina"
+# we can update the value if the data
